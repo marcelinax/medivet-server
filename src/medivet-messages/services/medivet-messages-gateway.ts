a4b052e5a4b04bbd762f2cdd6d35b9c1fd48264d
Marcelina Sitko <xmarcelasx@gmail.com>
2024-04-09 20:31:06 +0200

    emit websocket messages for proper users

--- src/medivet-messages/services/medivet-messages-gateway.ts
+++ src/medivet-messages/services/medivet-messages-gateway.ts
@@ -1,20 +1,40 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { plainToClass } from "class-transformer";
 import { Server, Socket } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
 import { MedivetMessage } from "@/medivet-messages/entities/medivet-message.entity";
 import { MedivetMessagesService } from "@/medivet-messages/services/medivet-messages.service";
 import { MedivetUser } from "@/medivet-users/entities/medivet-user.entity";
-import { MedivetUsersService } from "@/medivet-users/services/medivet-users.service";
 
 @WebSocketGateway(process.env.WEB_SOCKET_PORT ? Number(process.env.WEB_SOCKET_PORT) : 3006)
 export class MedivetMessagesGateway {
   @WebSocketServer() server: Server;
+  clients: Map<number, Socket> = new Map();
 
   constructor(
     private messagesService: MedivetMessagesService,
-    private usersService: MedivetUsersService
   ) {
+  }
+
+  async handleConnection(client: Socket): Promise<void> {
+      const clientId = plainToClass(MedivetUser, await this.messagesService.getUserFromSocket(client)).id;
+      this.clients.set(clientId, client);
+
+  }
+
+  async handleDisconnect(client: Socket): Promise<void> {
+      const clientId = plainToClass(MedivetUser, await this.messagesService.getUserFromSocket(client)).id;
+
+      this.clients.forEach((value, key) => {
+          if (key === clientId) {
+              this.clients.delete(key);
+          }
+      });
+  }
+
+  getClient(clientId: number): Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> {
+      return this.clients.get(clientId);
   }
 
   @SubscribeMessage("sendMessage")
@@ -26,15 +46,24 @@ export class MedivetMessagesGateway {
     },
   ): Promise<{ data: MedivetMessage }> {
       const issuer = plainToClass(MedivetUser, await this.messagesService.getUserFromSocket(socket));
+      const issuerSocket = this.getClient(issuer.id);
+      const receiverSocket = this.getClient(body.receiverId);
       const createdMessage = await this.messagesService.createMessage(issuer, {
           message: body.message,
           receiverId: body.receiverId
       });
-
-      this.server.emit("receiveMessage", {
-          message: "New message",
-          data: createdMessage
-      });
+      if (issuerSocket) {
+          issuerSocket.emit("receiveMessage", {
+              message: "New message",
+              data: createdMessage
+          });
+      }
+      if (receiverSocket) {
+          receiverSocket.emit("receiveMessage", {
+              message: "New message",
+              data: createdMessage
+          });
+      }
 
       return { data: createdMessage };
   };
@@ -46,11 +75,17 @@ export class MedivetMessagesGateway {
       receiverId: number;
     }
   ): Promise<{ data: MedivetMessage[] }> {
-      const issuer = await this.messagesService.getUserFromSocket(socket);
+      const issuer = plainToClass(MedivetUser, await this.messagesService.getUserFromSocket(socket));
+      const receiverSocket = this.getClient(body.receiverId);
+      const issuerSocket = this.getClient(issuer.id);
       const messages = await this.messagesService.markConversationAsRead(issuer, body.receiverId);
 
-      this.server.emit("receiveReadMessages", { data: messages });
-
+      if (issuerSocket) {
+          issuerSocket.emit("receiveReadMessages", { data: messages });
+      }
+      if (receiverSocket) {
+          receiverSocket.emit("receiveReadMessages", { data: messages });
+      }
       return { data: messages };
   }
 }
